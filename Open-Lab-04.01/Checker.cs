2ac6d6a3283233db98ac7c34c763c78081a36718
stark501 <mkudlacej@gmail.com>
2021-11-03 17:57:03 +0100

    update checker.cs

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+            if (str.ToCharArray()[i] == str.ToCharArray()[i + 1])
+            return true;
+            }
+            return false;
         }
     }
 }
